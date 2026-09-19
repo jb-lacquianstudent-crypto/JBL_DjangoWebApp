4057529059d7106af1ba4a7b970173473bfcf17c
eksodows <jedhernandez.student@ua.edu.ph>
2026-09-19 18:16:46 +0800

    first commit

---
+++ home/tests.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path(
+        "dynamic-form/",
+        views.dynamic_form,
+        name="dynamic_form"
+    ),
+]
+
+# Create your tests here.
